56789ec567d6f490e201b0a36f2df43263ff2f3b
Mark Lenser <mlenser@gmail.com>
2025-03-14 19:05:22 +0000

    refactor: rename tokens to base-tokens

--- scripts/_/validate-list.ts
+++ scripts/_/validate-list.ts
@@ -1,10 +1,10 @@
 import Ajv from 'ajv'
 import addFormats from 'ajv-formats'
 
+import type { BaseTokenListSchema } from '@/types/base-token-list'
 import type { GaugeListSchema } from '@/types/gauge-list'
 import type { GaugeTypesSchema } from '@/types/gauge-types'
 import type { ProtocolsSchema } from '@/types/protocols'
-import type { TokenListSchema } from '@/types/token-list'
 import type { ValidatorListSchema } from '@/types/validator-list'
 
 const ajv = new Ajv({ allErrors: true })
@@ -20,7 +20,7 @@ export const validateList = ({
     | GaugeListSchema
     | GaugeTypesSchema
     | ProtocolsSchema
-    | TokenListSchema
+    | BaseTokenListSchema
     | ValidatorListSchema
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   schema: any
