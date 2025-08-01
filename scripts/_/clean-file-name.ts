b2e708eafb44e4001a2d2ff7f572a28938c460bd
Mark Lenser <mlenser@gmail.com>
2025-08-01 12:11:33 +0100

    style: lint

--- scripts/_/clean-file-name.ts
+++ scripts/_/clean-file-name.ts
@@ -1,6 +1,6 @@
 export const cleanFileName = (fileName: string) =>
   `${fileName
-    .replace(/-\d+\.?\d*\%/, '') // kodiakTradingFee is not necessary as we don't need a different version of the images
+    .replace(/-\d+\.?\d*%/, '') // kodiakTradingFee is not necessary as we don't need a different version of the images
     .replace('.', '-')
     .replace('₮', 't')
     .replace(/\s|_|\/|\./g, '-')
