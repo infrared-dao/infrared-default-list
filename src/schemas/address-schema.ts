6faf79300e2c36e1b241fdb3a96ed6519133a8b1
Mikelle <mikhwall@gmail.com>
2025-12-26 20:22:19 +0100

    fix: lint

--- src/schemas/address-schema.ts
+++ src/schemas/address-schema.ts
@@ -1,11 +1,13 @@
 import { custom, pipe, string, transform } from 'valibot'
-import { isAddress, type Address } from 'viem'
+import { isAddress, isHex, type Address, type Hex } from 'viem'
+
+const isBytes32 = (val: string): boolean => isHex(val) && val.length === 66
 
 export const AddressSchema = pipe(
   string(),
   custom(
-    (val) => isAddress(val as string, { strict: false }),
-    'Enter a valid Address',
+    (val) => isAddress(val as string, { strict: false }) || isBytes32(val as string),
+    'Enter a valid Address or bytes32',
   ),
-  transform((val) => val.toLowerCase() as Address),
+  transform((val) => val.toLowerCase() as Address | Hex),
 )
